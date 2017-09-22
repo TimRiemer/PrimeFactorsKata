cfb20f362f1c362d54b51efc5557c458880bcf5d
Tim Riemer <tim.riemer@gmx.de>
2017-09-22 14:33:17 +0200

    PrimeFactorKata

---
+++ src/main/java/primeFactors/PrimeFactors.java
@@ -0,0 +1,20 @@
+package primeFactors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author tiri
+ * @version 0.1, 20.01.2015
+ */
+public class PrimeFactors {
+   public static List<Integer> generate(int n) {
+      List<Integer> primes = new ArrayList<Integer>();
+      for (int candidate = 2;n > 1; candidate++) {
+         for (; n % candidate == 0; n /= candidate) {
+            primes.add(candidate);
+         }
+      }
+      return primes;
+   }
+}
---
+++ src/test/java/primeFactors/PrimeFactorsTest.java
@@ -0,0 +1,58 @@
+package primeFactors;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static primeFactors.PrimeFactors.generate;
+
+/**
+ * @author tiri
+ * @version 0.1, 20.01.2015
+ */
+public class PrimeFactorsTest {
+   private List<Integer> list(int... ints) {
+      List<Integer> list = new ArrayList<Integer>();
+      for (int i : ints) {
+         list.add(i);
+      }
+      return list;
+   }
+
+   @Test
+   public void testOne() throws Exception {
+      assertEquals(list(), generate(1));
+   }
+
+   @Test
+   public void testTwo() throws Exception {
+      assertEquals(list(2), generate(2));
+   }
+
+   @Test
+   public void testThree() throws Exception {
+      assertEquals(list(3), generate(3));
+   }
+
+   @Test
+   public void testFour() throws Exception {
+      assertEquals(list(2,2), generate(4));
+   }
+
+   @Test
+   public void testSix() throws Exception {
+      assertEquals(list(2,3), generate(6));
+   }
+
+   @Test
+   public void testEight() throws Exception {
+      assertEquals(list(2,2,2), generate(8));
+   }
+
+   @Test
+   public void testNine() throws Exception {
+      assertEquals(list(3,3), generate(9));
+   }
+}
